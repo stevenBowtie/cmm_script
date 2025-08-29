98dcf362914acadb735cb099e26c8175e380092b
Paul Richard <prichard@creativemachines.com>
2025-08-28 22:20:14 -0700

    Create circle from selected points and print coordinates

--- points_to_circle.py
+++ points_to_circle.py
@@ -1,3 +1,11 @@
 import rhinoscriptsyntax as rs
-pts = rs.SelectedObjects()
-print( pts[0] )
+selPts = rs.SelectedObjects()
+pts = []
+for pt in selPts:
+    if rs.IsPoint:
+        coord = rs.PointCoordinates( pt )
+        pts.append( coord )
+rs.Command("Circle f")
+fitCirc = rs.LastCreatedObjects()
+print( rs.CircleCenterPoint( fitCirc[0] ) )
+print( pts )
