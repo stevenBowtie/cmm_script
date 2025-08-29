dfd9d6fbd1268a7ccfc246d4fc2ea149012ac994
Paul Richard <prichard@creativemachines.com>
2025-08-28 22:44:30 -0700

    Print the MIN/MAX/AVG deviation from a best-fit circle

--- points_to_circle.py
+++ points_to_circle.py
@@ -7,5 +7,19 @@ for pt in selPts:
         pts.append( coord )
 rs.Command("Circle f")
 fitCirc = rs.LastCreatedObjects()
-print( rs.CircleCenterPoint( fitCirc[0] ) )
-print( pts )
+centerPoint = rs.CircleCenterPoint( fitCirc[0] )
+rad = rs.CircleRadius( fitCirc )
+dist = []
+avg = 0
+for pt in pts:
+    ptDist = rs.Distance( centerPoint, pt )
+    dist.append( ptDist - rad )
+    avg += ptDist - rad
+minimus = min( dist )
+maximus = max( dist )
+average = avg / pts.__len__()
+print( "Radius: " + str( rs.CircleRadius( fitCirc ) ) )
+print( "Min: " + str( minimus ) )
+print( "Max: " + str( maximus ) )
+print( "Avg: " + str( average ) )
+#print( pts )
