e7850063e93abe2cafacc43c40a2587bcc1f05a6
Paul Richard <prichard@creativemachines.com>
2025-08-30 23:23:38 -0700

    Add script to turn points into line and report statistics

--- points_to_line.py
+++ points_to_line.py
@@ -1,30 +1,29 @@
 import rhinoscriptsyntax as rs
 selPts = rs.SelectedObjects()
 pts = []
+pts_coord = []
 for pt in selPts:
     if rs.IsPoint:
         coord = rs.PointCoordinates( pt )
         pts.append( coord )
-rs.Command("Circle f")
-fitCirc = rs.LastCreatedObjects()
-centerPoint = rs.CircleCenterPoint( fitCirc[0] )
-rs.AddPoint( centerPoint )
-rad = rs.CircleRadius( fitCirc )
+        pts_coord.append( coord )
+rs.Command("LineThroughPt")
+fitLine = rs.LastCreatedObjects()
 dist = []
 avg = 0
 for pt in pts:
-    ptDist = rs.Distance( centerPoint, pt )
-    dist.append( ptDist - rad )
-    avg += ptDist - rad
+    closest = rs.LineClosestPoint( fitLine, pt )
+    ptDist = rs.Distance( pt, closest )
+    dist.append( ptDist )
+    avg += ptDist
 minimus = min( dist )
 maximus = max( dist )
 average = avg / pts.__len__()
-print( "Diameter: " + str( rs.CircleRadius( fitCirc ) * 2 ) )
 print( "Min: " + str( minimus ) )
 print( "Max: " + str( maximus ) )
 print( "Avg: " + str( average ) )
 
-for pt in range( selPts.__len__() ):
+for pt in range( pts.__len__() ):
     if( dist[pt] < 0 ):
         h = 85 - int( dist[pt] / minimus  * 170 )
     else:
