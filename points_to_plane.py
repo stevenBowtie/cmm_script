24bf26ebf36e29bd4d1ce6d0c3cf08dc4a352224
Paul Richard <prichard@creativemachines.com>
2025-09-05 21:08:53 -0700

    Fix points-to-plane, add coloring
    Kind of a hack using the command instead of the Python function because
    of issues with PlaneFitFromPoints creating a small plane.
    PlaneThroughtPoints doesn't actually make a plane, it makes a surface
    so there are extra steps in the distance calculations.

--- points_to_plane.py
+++ points_to_plane.py
@@ -6,14 +6,26 @@ for pt in selPts:
         pts.append( pt )
 rs.Command( "PlaneThroughPt" )
 pln = rs.LastCreatedObjects()
-#pln = rs.PlaneFitFromPoints( pts )
-#if pln:
-#    magX = pln.XAxis.Length
-#    magY = pln.YAxis.Length
-#    rs.AddPlaneSurface( pln, magX, magY )
 deviations = []
+avg = 0
 for pt in pts:
-    closest = rs.PlaneClosestPoint( pln, pt )
-    dev = rs.Distance( pt, closest )
+    closest = rs.SurfaceClosestPoint( pln, pt )
+    p3d = rs.EvaluateSurface( pln, closest[0], closest[1] )
+    dev = rs.Distance( pt, p3d )
     deviations.append( dev )
+    avg += dev
 print( deviations )
+minimus = min( deviations )
+maximus = max( deviations )
+average = avg / deviations.__len__()
+print( "Min: " + str( minimus ) )
+print( "Max: " + str( maximus ) )
+print( "Avg: " + str( average ) )
+
+for pt in range( pts.__len__() ):
+    if( deviations[pt] < 0 ):
+        h = 85 - int( deviations[pt] / minimus  * 170 )
+    else:
+        h = 85 - int( deviations[pt] / maximus * 170 )
+    rs.ObjectColor( selPts[pt], rs.ColorHLSToRGB( [ h, 100, 255] ) )
+rs.UnselectAllObjects()
